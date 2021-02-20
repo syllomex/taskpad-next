94a388cd21d6fa665ee1c534607cc17e093d39ca
syllomex <leonardojdossantos@gmail.com>
2021-02-20 20:58:52 -0300

    chore: add fonts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,44 @@
 import { NextPage } from "next";
+import Head from "next/head";
 import React from "react";
 
-const App: NextPage = () => <h1>Hello World!</h1>;
+const App: NextPage = () => (
+  <div>
+    <Head>
+      <link
+        rel="preload"
+        href="/fonts/Poppins/Poppins-Regular.ttf"
+        as="font"
+        crossOrigin=""
+      />
+      <link
+        rel="preload"
+        href="/fonts/Poppins/Poppins-Medium.ttf"
+        as="font"
+        crossOrigin=""
+      />
+      <link
+        rel="preload"
+        href="/fonts/Poppins/Poppins-Bold.ttf"
+        as="font"
+        crossOrigin=""
+      />
+      <link
+        rel="preload"
+        href="/fonts/Rajdhani/Rajdhani-Regular.ttf"
+        as="font"
+        crossOrigin=""
+      />
+      <link
+        rel="preload"
+        href="/fonts/Rajdhani/Rajdhani-Bold.ttf"
+        as="font"
+        crossOrigin=""
+      />
+      <title>TaskPad Next</title>
+    </Head>
+    <h1>TaskPad Next</h1>
+  </div>
+);
 
 export default App;
