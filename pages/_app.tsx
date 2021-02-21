d1185ec6f26dd302d2886c8d41571f8b680891af
syllomex <leonardojdossantos@gmail.com>
2021-02-21 12:02:10 -0300

    feat: add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable react/prop-types */
 
+import Head from "next/head";
 import React from "react";
 import PageProvider from "../store/page";
 
@@ -9,6 +10,28 @@ import "../styles/globals.css";
 function MyApp({ Component, pageProps }) {
   return (
     <PageProvider>
+      <Head>
+        <link
+          rel="preload"
+          href="/fonts/Poppins/Poppins-Regular.ttf"
+          as="font"
+          crossOrigin=""
+        />
+        <link
+          rel="preload"
+          href="/fonts/Poppins/Poppins-Medium.ttf"
+          as="font"
+          crossOrigin=""
+        />
+        <link
+          rel="preload"
+          href="/fonts/Poppins/Poppins-Bold.ttf"
+          as="font"
+          crossOrigin=""
+        />
+        <title>TaskPad Next</title>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
       <Component {...pageProps} />
     </PageProvider>
   );
