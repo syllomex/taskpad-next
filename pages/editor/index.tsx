1415297b8f206c5493c30bf3737f34092be111af
syllomex <leonardojdossantos@gmail.com>
2021-02-21 11:39:33 -0300

    feat: add editor page

---
+++ pages/editor/index.tsx
@@ -0,0 +1,37 @@
+import Head from "next/head";
+import React from "react";
+
+import Nav from "../../components/Nav";
+import Title from "../../components/Title";
+import Wrapper from "../../components/Wrapper";
+
+import { usePage } from "../../store/page";
+
+import { isServer } from "../../utils/isServer";
+
+import { Container } from "./styles";
+
+const Editor: React.FC = () => {
+  const { current, get } = usePage();
+
+  return (
+    <Wrapper>
+      <Head>
+        <title>
+          {isServer
+            ? "TaskPad"
+            : `${get(current) ? `${get(current).name} - TaskPad` : "TaskPad"}`}
+        </title>
+      </Head>
+      <Nav />
+
+      {current && (
+        <Container>
+          <Title />
+        </Container>
+      )}
+    </Wrapper>
+  );
+};
+
+export default Editor;
